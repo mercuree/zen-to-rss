3704cd3f948b2cf452e5ee962a89e0e823841457
mercuree <mercuree.lab@gmail.com>
2019-02-16 20:06:03 +0200

    Parsing changed

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from flask import Flask
 from flask import request
 from flask import Response
 from flask import abort
-from datetime import datetime
-from datetime import timezone
+import dateparser
 from urllib.parse import urlparse
 from urllib.parse import quote_plus
 import re
@@ -49,7 +48,7 @@ def get_rss():
     if tg_rhash and not re.match(r'^[a-fA-F\d]+$', tg_rhash):
         return 'Invalid tg_rhash. Please, check rhash value from instant view template'
 
-    if not re.match(r'^/media/(id/[\da-f]+|[a-z\d_]+)/?$', parsed_url.path):
+    if not re.match(r'^/(media/)?(id/[\da-f]+|[a-z\d_]+)/?$', parsed_url.path):
         return 'Url is unsupported. Supported formats:<br>' \
                '• https://zen.yandex.ru/media/id/01234567890abcdef0123456 <br>' \
                '• https://zen.yandex.ru/media/nickname'
@@ -60,67 +59,52 @@ def get_rss():
     doc = fromstring(resp.text)
 
     try:
-        json_data = json.loads(doc.xpath('.//*[@id="init_data"]')[0].text)
+        text = re.search(r'{.+}', doc.xpath('.//script[contains(text(), "window.__SERVER_STATE__")]')[0].text)[0]
+        json_data = json.loads(text)
     except:
         return abort(404)
 
-    items = json_data.get('publications')
-    publisher = json_data.get('publisher')
+    items = json_data['feed'].get('items')
+    items_order = json_data['feed'].get('itemsOrder')
+    publisher = next(iter(json_data.get('sources').values()))
 
     feed = FeedGenerator()
     feed.id('http://zen.yandex.ru/')
-    feed.title(publisher.get('name'))
+    feed.title(publisher.get('title'))
     feed.subtitle(publisher.get('description').strip())
     feed.language('ru')
     feed.author({'name': '-', 'email': '-'})
     feed.link(href=zen_url, rel='alternate')
     try:
-        image_logo_id = json_data['publisher']['logo'].get('id')
-        image_logo = json_data['images'].get(image_logo_id)
-        image_logo_url = IMAGE_URL.format(
-            namespace=image_logo['namespace'],
-            groupId=image_logo['groupId'],
-            imageName=image_logo['imageName']
-        )
+        image_logo_url = publisher.get('logo')
         feed.logo(image_logo_url)
     except:
         pass
 
-    for item in items:
+    for oItem in items_order:
+        item = items.get(oItem)
+        if item.get('type') != 'card':
+            continue
 
         entry = feed.add_entry()
 
         entry.title(
-            item['content']['preview'].get('title').strip()
+            item.get('title').strip()
         )
 
         entry.description(
-            item['content']['preview'].get('snippet').strip()[:limit_description]
+            item.get('text').strip()[:limit_description]
         )
 
-        if item['content']['preview'].get('image'):
-            item_image = json_data['images'].get(
-                item['content']['preview']['image'].get('id')
-            )
-
-            item_image_url = IMAGE_URL.format(
-                namespace=item_image['namespace'],
-                groupId=item_image['groupId'],
-                imageName=item_image['imageName']
-            )
+        if item.get('image'):
+            item_image_url = item.get('image')
             entry.enclosure(
                 url=item_image_url,
-                type='image/%s' % item_image['meta']['origFormat'].lower(),
+                type='image/webp',
                 length='2048'
             )
 
-        # set /media/<nickname> if available, otherwise set /media/id/<publisherId>
-        publisher_identity = publisher['nickname'].get('normalized') if 'nickname' in publisher else "id/" + item.get('publisherId')
-        entry_url = ENTRY_URL.format(
-            publisherId=publisher_identity,
-            titleForUrl=item.get('titleForUrl'),
-            id=item.get('id')
-        )
+        entry_url = item.get('link').split('?')[0]
         # convert to instant view link if tg hash is provided
         if tg_rhash:
             # write original url into author field
@@ -130,9 +114,12 @@ def get_rss():
         else:
             entry.link({'href': entry_url})
 
-        entry.pubdate(
-            datetime.fromtimestamp(item['content'].get('modTime') / 1000).astimezone()
-        )
+        try:
+            entry.pubdate(
+                dateparser.parse(item.get('creationTime'), settings={'RETURN_AS_TIMEZONE_AWARE': True})
+            )
+        except:
+            pass
 
     rss_response = Response(feed.rss_str(pretty=True))
     rss_response.headers.set('Content-Type', 'application/rss+xml; charset=utf-8')
